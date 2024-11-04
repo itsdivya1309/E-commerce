f559447ef47c787499f91f3bf8a685b76b95e80e
Divya Tiwari <gungun130903@gmail.com>
2024-11-04 19:50:55 +0530

    Frontend

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Categories)
+admin.site.register(Products)
+admin.site.register(Customers)
+admin.site.register(Orders)
+admin.site.register(OrderItems)
+admin.site.register(Reviews)
+admin.site.register(Payments)
+admin.site.register(Cart)
