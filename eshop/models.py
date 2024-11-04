74d185e8292fc18530ebec199ad29792f59ea7bd
Prachisood23 <prachi.sood.bce21@itbhu.ac.in>
2024-11-04 19:14:16 +0530

    'hello'

--- eshop/models.py
+++ eshop/models.py
@@ -4,17 +4,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 # A table to store user login data
+from django.contrib.auth.models import User
+from django.db import models
+
 class Customers(models.Model):
     customer_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone_no = models.IntegerField()
     email = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
     shipping_address = models.TextField()
     billing_address = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)  # Link to User model
+
     def __str__(self):
-        return self.first_name + self.last_name
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
     
 # Product Category
 class Categories(models.Model):
